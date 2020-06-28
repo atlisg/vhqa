80a067ea78fd70aefe80acfffa4d09b180a2a7e8
atlisg <atlisg@ru.is>
2020-06-28 14:04:41 +0000

    use the models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Answer;
+use App\Question;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
   public function index()
   {
-    $questions = DB::table('questions')->get();
-
-    foreach ($questions as $question)
-      $question->answers = DB::table('answers')->where('question_id', $question->id)->get();
+    $questions = Question::get();
 
     return view('layout.mainlayout', ['questions' => $questions]);
   }
